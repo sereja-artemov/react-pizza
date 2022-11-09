7f7a3b2de78bce2bd4d6162a84c6f7daa2e761d2
sereja-art <sereja.artemov.art@yandex.ru>
2022-11-09 22:26:05 +0300

    features: Создать компоненты, SCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import './scss/app.scss';
+// @ts-ignore
+import Header from './components/Header';
+import Sort from './components/Sort';
+import Categories from './components/Categories';
+import PizzaCard from './components/PizzaCard';
 
 function App() {
   return (
-    <div className="App">
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <PizzaCard title="Мексиканская" price={500} />
+            <PizzaCard title="Итальянская" price={600} />
+            <PizzaCard title="Европейская" price={550} />
 
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
